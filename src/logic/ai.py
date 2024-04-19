d7d207cbc9eacecda1bd411789d58f1542782871
oskera <oskari.e.keranen@gmail.com>
2024-04-20 02:06:38 +0300

    ai: add method for next action

--- src/logic/ai.py
+++ src/logic/ai.py
@@ -1,15 +1,37 @@
 """AI for for rock-paper-scissors games"""
 
+import re
+
+
 class AI:
     """Single AI with n-length memory"""
 
     game = None
     n = 0
-    moves = ['R', 'P', 'S']
+    actions = ['R', 'P', 'S']
 
     def __init__(self, game, n):
         self.game = game
         self.n = n
 
-    def get_move(self):
-        return None
+    def get_action(self):
+        recent = self.get_recent()
+        frequency = [0] * len(self.actions)
+        for i in range(0, len(self.actions)):
+            frequency[i] = self.count(recent + self.actions[i])
+        next = self.actions[frequency.index(max(frequency))]
+        return self.get_counter(next)
+
+    def get_counter(self, action):
+        if action == 'R':
+            return 'P'
+        elif action == 'P':
+            return 'S'
+        elif action == 'S':
+            return 'R'
+
+    def get_recent(self):
+        return self.game.action_history[-self.n:]
+
+    def count(self, sequence):
+        return len(re.findall(f'(?={sequence})', self.game.action_history))
--- src/rps.py
+++ src/rps.py
@@ -1,10 +1,13 @@
 """Main module"""
 
 from logic.game import Game
+from logic.ai import AI
 
 game = Game()
+ai = AI(game, 1)
 game.play('R', 'P')
-game.play('P', 'R')
-game.play('S', 'P')
+game.play('R', 'R')
+game.play('R', 'R')
 game.play('P', 'P')
-print(game.action_history)
+game.play('R', 'P')
+print(ai.get_action())
