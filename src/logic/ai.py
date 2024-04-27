ed8d881d75e4c52fa22a2223c819226abf3e2658
oskera <oskari.e.keranen@gmail.com>
2024-04-28 02:14:49 +0300

    ai: add method for determining best single ai

--- src/logic/ai.py
+++ src/logic/ai.py
@@ -18,10 +18,17 @@ class MultiAI:
         for i in range(1, f + 1):
             self.singles = self.singles + [AI(self.game, i)]
 
+    def get_best(self):
+        winrate = []
+        for ai in self.singles:
+            winrate = winrate + [ai.get_win_rate()]
+        return winrate.index(max(winrate))
+
 class AI:
     """Single AI with n-length memory"""
 
     game = None
+    action_history = ""
     n = 0
     actions = ['R', 'P', 'S']
 
@@ -30,14 +37,17 @@ class AI:
         self.n = n
 
     def get_action(self):
-        recent = self.get_recent()
+        recent = self.get_recent_opponent()
         if len(recent) < self.n:
-            return self.get_random()
-        frequency = [0] * len(self.actions)
-        for i in range(0, len(self.actions)):
-            frequency[i] = self.count(recent + self.actions[i])
-        next = self.actions[frequency.index(max(frequency))]
-        return self.get_counter(next)
+            action = self.get_random()
+        else:
+            frequency = [0] * len(self.actions)
+            for i in range(0, len(self.actions)):
+                frequency[i] = self.count(recent + self.actions[i])
+            next = self.actions[frequency.index(max(frequency))]
+            action = self.get_counter(next)
+        self.update_history(action)
+        return action
 
     def get_counter(self, action):
         if action == 'R':
@@ -47,8 +57,25 @@ class AI:
         elif action == 'S':
             return 'R'
 
-    def get_recent(self):
+    def update_history(self, action):
+        self.action_history = self.action_history + action
+
+    def get_recent_self(self):
+        return self.action_history[-self.n:]
+
+    def get_recent_opponent(self):
         return self.game.action_history[-self.n:]
+
+    def get_win_rate(self):
+        recent_self = self.get_recent_self()
+        recent_opponent = self.get_recent_opponent()
+        wins = 0
+        for i in range(0, self.n):
+            action_self = recent_self[i]
+            action_opponent = recent_opponent[i]
+            if self.game.result(action_self, action_opponent) == 1:
+                wins = wins + 1
+        return wins/self.n
 
     def count(self, sequence):
         return len(re.findall(f'(?={sequence})', self.game.action_history))
