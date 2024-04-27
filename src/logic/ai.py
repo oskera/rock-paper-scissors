d98a246ff47e047458779d626826ba41efc9376b
oskera <oskari.e.keranen@gmail.com>
2024-04-28 01:27:22 +0300

    ai: add multi ai

--- src/logic/ai.py
+++ src/logic/ai.py
@@ -1,7 +1,22 @@
 """AI for for rock-paper-scissors games"""
 
+import random
 import re
 
+
+class MultiAI:
+    """AI consisting of multiple single AIs"""
+
+    game = None
+    singles = []
+
+    def __init__(self, game, f):
+        self.game = game
+        self.initialize_singles(f)
+
+    def initialize_singles(self, f):
+        for i in range(1, f + 1):
+            self.singles = self.singles + [AI(self.game, i)]
 
 class AI:
     """Single AI with n-length memory"""
@@ -16,6 +31,8 @@ class AI:
 
     def get_action(self):
         recent = self.get_recent()
+        if len(recent) < self.n:
+            return self.get_random()
         frequency = [0] * len(self.actions)
         for i in range(0, len(self.actions)):
             frequency[i] = self.count(recent + self.actions[i])
@@ -35,3 +52,6 @@ class AI:
 
     def count(self, sequence):
         return len(re.findall(f'(?={sequence})', self.game.action_history))
+
+    def get_random(self):
+        return self.actions[random.randint(0,len(self.actions) - 1)]
